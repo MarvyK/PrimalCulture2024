a205d6cd434801a104103c7b336cebd6ccb69d87
MarvyK <marvin.poole@gmail.com>
2024-10-29 00:05:05 +0200

    Updated styles reference in Layout.astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://marvyk.github.io/PrimalCulture2024/',
-    base: '/PrimalCulture2024/',
+    
 });
