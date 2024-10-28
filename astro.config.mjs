9c00994d11b59ed2254b7c2ed039d8815f5000f6
MarvyK <marvin.poole@gmail.com>
2024-10-28 23:04:23 +0200

    Updated astro.config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://marvyk.github.io/PrimalCulture2024/',
+});
