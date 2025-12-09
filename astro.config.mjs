ee3d9a9d2a9b14ece7fa1fe6574dd036c9c4a9f9
Marvin Poole <marvin.poole@gmail.com>
2025-12-09 23:43:36 +0200

    Static output

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 import { defineConfig } from "astro/config";
 
+// export default defineConfig({
+//   site: "https://marvyk.github.io",
+//   base: "/PrimalCulture2024",
+//   image: {
+//     service: {
+//       entrypoint: "astro/assets/services/sharp",
+//     },
+//   },
+// });
 export default defineConfig({
-  site: "https://marvyk.github.io",
-  base: "/PrimalCulture2024",
-  image: {
-    service: {
-      entrypoint: "astro/assets/services/sharp",
-    },
-  },
+  output: "static",
 });
